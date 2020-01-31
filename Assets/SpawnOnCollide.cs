7f777af2897515779cc4baf807abd66b45b7b34c
Lucas <lucas.leplat@gmail.com>
2020-02-01 00:59:51 +0100

    InitialPush
    Hop

---
+++ Assets/SpawnOnCollide.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnCollide : MonoBehaviour
+{
+	public GameObject spawnedFX;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnCollisionEnter (Collision collision)
+	{
+		GameObject fx = Instantiate(spawnedFX, collision.contacts[0].point, transform.rotation);
+		Destroy(fx, 5f);
+	}
+}
