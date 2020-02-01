0ace196f9c144a65e72f172081042f54e9e3844e
Kenny Ramin <kenny.ramin433@gmail.com>
2020-02-01 16:50:10 +0100

    Correction orthographe et langue des écrans de Victoire et Defaite + ajout du squelette de changement d'état par palier

---
+++ Assets/Scripts/ChangementTowerState.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangementTowerState : MonoBehaviour
+{
+    public GameObject player;
+    public int nbOfStates;
+    private int multiplier;
+    private int nbPickupObjective;
+    private int nbPickupFinal;
+    List<int> state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = new List<int>();
+        generateList();
+        nbPickupFinal = player.GetComponent<VictoryGameMenu>().nbPickupFinal;
+        multiplier = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        towerState();
+    }
+
+    void generateList()
+    {
+        for (int i = 0; i <= nbOfStates; i++)
+        {
+            state.Add(nbPickupFinal / nbOfStates * multiplier + 1);
+        }
+    }
+
+    void towerState()
+    {
+        nbPickupObjective = player.GetComponent<VictoryGameMenu>().GetNbPickupOjective();
+
+        foreach (int s in state)
+        {
+            if (nbPickupObjective == s)
+            {
+                //Recupere les niveau de meshs en fonction
+            }
+        }
+    }
+}
