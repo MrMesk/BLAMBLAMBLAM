1439667f6048b6fd3c6ec6a6d3bf930d4669d54b
Kenny Ramin <kenny.ramin433@gmail.com>
2020-02-01 02:33:43 +0100

    end game

---
+++ Assets/Scripts/conditionForWin.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class conditionForWin : MonoBehaviour
+{
+    public int nbPickupFinal = 10;
+    private int nbPickupOjective;
+    private bool trigger;
+    public Collider constructZone;
+    public CharacterController player;
+    public PlayerInventory inventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nbPickupOjective = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        endOfTheGame();
+
+    }
+
+    private void endOfTheGame()
+    {
+        if (trigger)
+        {
+            nbPickupOjective += inventory.getPickupCount();
+            inventory.gainPickup(-inventory.getPickupCount());
+            if (nbPickupFinal == nbPickupOjective)
+            {
+                Debug.Log("Win");
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        inventory = other.GetComponent<PlayerInventory>();
+        if (inventory != null)
+        {
+            trigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerInventory>() != null)
+        {
+            trigger = false;
+            inventory = null;
+        }
+    }
+
+
+
+}
