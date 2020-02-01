9b1928e57d3b567e4cb0dee65e10e0de8b2c7b86
Alexaline Thomas <thomas.alexaline@etu.u-bordeaux.fr>
2020-02-01 12:29:44 +0100

    import package du cancer + début outOfBound

---
+++ Assets/Scripts/SimpleTestCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTestCharacter : MonoBehaviour
+{
+	[SerializeField] float moveSpeed = 5f;
+	Rigidbody r;
+    // Start is called before the first frame update
+    void Start()
+    {
+		r = GetComponent<Rigidbody>();
+    }
+
+	Vector3 GetMoveDir()
+	{
+		Vector3 moveDir = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+		return moveDir;
+	}
+    // Update is called once per frame
+    void Update()
+    {
+		transform.Translate(GetMoveDir() * Time.deltaTime * moveSpeed);
+    }
+}
