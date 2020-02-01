21837e4bc193d518dbc732b37aabd467a77dfe6e
Lucas <lucas.leplat@gmail.com>
2020-02-01 22:23:49 +0100

    Fix le cancer

---
+++ Assets/Scripts/DamageOnTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageOnTrigger : MonoBehaviour
+{
+	public float appliedForceOnHit = 5f;
+	public GameObject spawnedFX;
+	public float distanceOffset = 0.5f;
+	public LayerMask platformLayerMask;
+
+	public bool destroyOnImpact = false;
+
+	private void OnTriggerEnter (Collider other)
+	{
+		Debug.Log("Trigger enter !");
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position + transform.up * distanceOffset, transform.up * -1f,out hit, distanceOffset * 4f, platformLayerMask))
+		{
+			Rigidbody r = hit.collider.attachedRigidbody;
+
+			PlatformLife life = other.GetComponent<PlatformLife>();
+
+			if(life)
+			{
+				life.ModifLifeValue(-1);
+			}
+
+			r.AddForceAtPosition(transform.up * -1f * appliedForceOnHit, hit.point);
+			if(destroyOnImpact)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+}
