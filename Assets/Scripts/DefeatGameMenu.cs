3f4af163b5179450d37ab2949917690841ae8b2c
Kenny Ramin <kenny.ramin433@gmail.com>
2020-02-01 14:35:41 +0100

    ajout de la victoire et de la defaite , mais un bug en cour de correction pour l'ecran de défaite

---
+++ Assets/Scripts/DefeatGameMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefeatGameMenu : MonoBehaviour
+{
+   
+    private float timer;
+    private Clock clock= new Clock();
+    public GameObject menu;
+    private bool isEnd;
+
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        clock.timerDecrementation();
+        Debug.Log(clock.timer);
+        activeMenu();
+    }
+
+    public void activeMenu()
+    {
+        timer = clock.timer;
+        menu.SetActive(isEnd);
+        if (timer <= 0)
+        {
+            isEnd = true;
+            
+        }
+        else
+        {
+            isEnd = false;
+        }
+       
+    }
+
+    private void OnGUI()
+    {
+        if (timer <= 0)
+        {
+            GUI.Box(new Rect(Screen.width / 2 - 60, Screen.height / 2 - 120, 180, 250), "Defeat");
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2-50, 160, 50), "Retry"))
+            {
+                Application.LoadLevel("level1");
+            }
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 10, 160, 50), "Retour au menu principal"))
+            {
+                Application.LoadLevel("Main menu");
+            }
+        }
+    }
+}
--- Assets/Scripts/VictoryGameMenu.cs
+++ Assets/Scripts/VictoryGameMenu.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class conditionForWin : MonoBehaviour
+public class VictoryGameMenu : MonoBehaviour
 {
     public int nbPickupFinal = 10;
     private int nbPickupOjective;
     private bool trigger;
     public Collider constructZone;
-    public CharacterController player;
-    public PlayerInventory inventory;
+    public GameObject player;
+    private PlayerInventory inventory;
+    private Clock clock = new Clock();
 
     // Start is called before the first frame update
     void Start()
@@ -18,23 +19,18 @@ public class conditionForWin : MonoBehaviour
         nbPickupOjective = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        clock.timerDecrementation();
         endOfTheGame();
-
     }
-
+    // Update is called once per frame
     private void endOfTheGame()
     {
         if (trigger)
         {
             nbPickupOjective += inventory.getPickupCount();
             inventory.gainPickup(-inventory.getPickupCount());
-            if (nbPickupFinal == nbPickupOjective)
-            {
-                Debug.Log("Win");
-            }
         }
     }
 
@@ -53,6 +49,22 @@ public class conditionForWin : MonoBehaviour
         {
             trigger = false;
             inventory = null;
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (nbPickupFinal == nbPickupOjective && clock.timer>0)
+        {
+            GUI.Box(new Rect(Screen.width / 2 - 60, Screen.height / 2 - 120, 180, 250), "Victory");
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 160, 50), "Next Level"))
+            {
+                Application.LoadLevel("level1");
+            }
+            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 10, 160, 50), "Retour au menu principal"))
+            {
+                Application.LoadLevel("Main menu");
+            }
         }
     }
 
